38d4bd42eaa3f14b5f41b61386cd3b5702bbb979
oowoo123 <togun003@gmail.com>
2023-10-08 14:38:37 +0100

    scripts

---
+++ myfrontend/script.js
@@ -0,0 +1,13 @@
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "Basic ZXhhbXBsZV91c2VybmFtZTpleGFtcGxlX3Bhc3N3b3Jk");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("http://127.0.0.1:5000/interview_questions?topic=Communication", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
